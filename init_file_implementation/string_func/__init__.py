a23ef5c85917958e1a824453805681ab79311376
raylai <chunjueilai@gmail.com>
2025-07-07 22:01:03 +0800

    fix: Add __init__.py for main to import

---
+++ init_file_implementation/string_func/__init__.py
@@ -0,0 +1,9 @@
+from .string_length import get_length
+from .string_to_lowercase import get_lower
+from .string_to_uppercase import get_upper
+
+__all__ = [
+    "get_length",
+    "get_lower",
+    "get_upper",
+]
